386ebc252366869c731b3e00329ba7a54c6e2a47
e-foley <efoley@calpoly.edu>
2019-10-08 20:24:45 -0700

    first hall switch class

---
+++ libraries/HallSwitch/hall_switch.h
@@ -0,0 +1,18 @@
+#ifndef HALL_SWITCH_H_
+#define HALL_SWITCH_H_
+
+class HallSwitch {
+ public:
+  HallSwitch(int power_pin, int state_pin);
+  void init();
+  bool isInitialized() const;
+  void setPowerState(bool power_state);
+  bool isTriggered() const;
+
+ private:
+   int power_pin_;
+   int state_pin_;
+   bool is_initialized_;
+};
+
+#endif
