4402955a1a001ebdf50aae495dbd43b5986d6969
e-foley <efoley@calpoly.edu>
2019-10-09 12:48:33 -0700

    attempt to document index task
    I'm in glare city, so I should review this later...

--- libraries/IndexTask/index_task.h
+++ libraries/IndexTask/index_task.h
@@ -3,10 +3,17 @@
 
 #include "hall_switch.h"
 #include "mask_controller.h"
-#include <Arduino.h>
 
+// Operates a cooperative task whose responsibility is to drive a MaskController
+// and HallSwitch in conjunction to determine a new index position for the mask.
+// Physically, this index position is determined a location of peak magnetic
+// field. No other functions should attempt to manipulate the HallSwitch,
+// MaskController, or the MaskController's dependencies while indexing is
+// active.
 class IndexTask {
  public:
+
+  // List of possible states the IndexTask can be in.
   enum class State : int {
     START = 0,
     INIT,
@@ -19,25 +26,63 @@ class IndexTask {
     CANNOT_INDEX
   };
 
-  static const int INDEX_TIMEOUT_MS = 10000u;  // [ms]
+  // Amonut of time we are willing to wait for a HallSwitch state transition
+  // befire declaring that the device is  unable to find an index [ms].
+  static const int INDEX_TIMEOUT_MS = 10000u;
 
+  // Construct a new IndexTask, designating  the MaskController and
+  // HallSwitch the task will operate.
+  //
+  // mask_controller: The MaskController to operate.
+  // hall_switch: The HallSwitch to read.
   IndexTask(MaskController* mask_controller, HallSwitch* hall_switch);
+
+  // Initialize the IndexTask. This must be requested before calling index().
   void init();
+
+  // Checks for state transitions and takes actions accordingly. Call this as
+  // frequently as possible to improve indexing resolution.
   void step();
+
+  // Seek an index position for the mask. An index position will be established
+  // where the task estimates a local peak in magnetic field strength, which
+  // will typically be triggered when the magnet is directly above the physical
+  // Hall effect sensor. Note that this operation will change the index of the
+  // MaskController, affecting all subsequent MaskController actions.
   void index();
+
+  // Retrieves the current state of the IndexTask. See the State enumeration.
   State getState() const;
 
  private:
+  // Length of array in which we store positions to use in calculating an
+  // index position.
   static const size_t NUM_KEY_POSITIONS = 4u;
 
+  // Utility method to check when an index timeout has occurred.
+  //
+  // Returns: Irue if a timeout is active.
   bool timedOut() const;
 
+  // The MaskController to manipulate..
   MaskController* const mask_controller_;
+
+  // The HallSwitch to read.
   HallSwitch* const hall_switch_;
+
+  // Flags for requested actions.
   bool init_requested_;
   bool index_requested_;
+
+  // Current state of the IndexTask.
   State state_;
+
+  // Time of last HallSwitch state change or request to index. Used as a
+  // reference for index timeouts.
   uint32_t last_index_progress_stamp_ms_;
+
+  // Container for angle datapoints used in the determination of the True
+  // index position.
   float key_positions_deg_[NUM_KEY_POSITIONS];
 };
 
