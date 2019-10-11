d89c85159329bacdf3b8cf5e20548c4ede3e7a4c
e-foley <efoley@calpoly.edu>
2019-10-11 10:47:35 -0700

    some docs in mask rotator

--- mask_rotator/mask_rotator.ino
+++ mask_rotator/mask_rotator.ino
@@ -6,38 +6,43 @@
 #include "stepper_controller.h"
 #include "timer_one.h"
 
+// Serial config
 const int SERIAL_BAUD_RATE = 19200;
 const int SERIAL_TIMEOUT_MS = 10;  // [ms]
-const float GEAR_RATIO = 72.0/17.0;
-const int16_t MOTOR_STEPS = 200u;
+
+// Motor/mask config
 const int BRKA_PIN = 9;
 const int DIRA_PIN = 12;
 const int PWMA_PIN = 3;
 const int BRKB_PIN = 8;
 const int DIRB_PIN = 13;
 const int PWMB_PIN = 11;
+const float GEAR_RATIO = 72.0/17.0;
+const int16_t MOTOR_STEPS = 200u;  // Motor steps per revolution
 uint32_t STEP_PERIOD_US = 8000u;  // [us]
 const MaskController::Direction PREFERRED_DIRECTION =
     MaskController::Direction::AUTO;
+
+// Hall switch config
 const int HALL_SWITCH_POWER_PIN = 4;
 const int HALL_SWITCH_STATE_PIN = 5;
 
+// Objects, state variables, etc.
 BipolarStepper stepper(BRKA_PIN, DIRA_PIN, PWMA_PIN, BRKB_PIN, DIRB_PIN, PWMB_PIN);
 HallSwitch hall_switch(HALL_SWITCH_POWER_PIN, HALL_SWITCH_STATE_PIN);
 StepperController motor_controller(&stepper, MOTOR_STEPS);
 MaskController mask_controller(&motor_controller, GEAR_RATIO);
 IndexTask index_task(&mask_controller, &hall_switch);
 TimerOne timer;
-
 enum class Mode {
   NONE,
   ABSOLUTE,
   RELATIVE
 } mode = Mode::ABSOLUTE;
-
 float target = 0.0f;
 bool dir = true;
 
+// Called once at the start of the progrom; initializes all hardware and tasks.
 void setup() {
   Serial.begin(SERIAL_BAUD_RATE);
   Serial.setTimeout(SERIAL_TIMEOUT_MS);
@@ -49,6 +54,8 @@ void setup() {
   timer.attachInterrupt(update, STEP_PERIOD_US);
 }
 
+// Called repeatedly: updates tasks and looks for new actions to take based on
+// command inputs.
 void loop() {
   index_task.step();
 
